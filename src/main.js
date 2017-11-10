d4dd87d248710f9929c22ed92fc8dcde6053b1dc
xiaolin <462441355@qq.com>
2017-11-10 10:49:02 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
+// 懒加载
+import VueLazyload from 'vue-lazyload'
+import router from './router'
+
+Vue.use(ElementUI, { locale })
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
