46542d7741b0f0f7cc548975ebd1e3cdd3f6faad
xiaolin <462441355@qq.com>
2017-11-24 14:00:33 +0800

    * 页面基础搭建

--- src/components/excel/index.vue
+++ src/components/excel/index.vue
@@ -1,5 +1,15 @@
 <template>
-	<div>
-    sss
+	<div class="app-container">
+    <el-button style='margin-bottom:20px;' type="primary" icon="document" @click="handleDownload" :loading="downloadLoading">导出excel</el-button>
   </div>
 </template>
+
+<script>
+  export default {
+    methods:{
+      handleDownload(){
+
+      }
+    }
+  }
+</script>
