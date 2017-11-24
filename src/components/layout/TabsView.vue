3bfd5f433df8edaf67ed476829930275dc53b34b
xiaolin <462441355@qq.com>
2017-11-24 10:38:31 +0800

    * 路由监听

--- src/components/layout/TabsView.vue
+++ src/components/layout/TabsView.vue
@@ -7,8 +7,25 @@
 <script>
 export default {
   computed: {},
-  methods: {},
-  watch: {}
+  methods: {
+    // 添加路由
+    addViewTabs() {
+      const route = this.generateRoute();
+      console.log(route);
+    },
+    // 获取route
+    generateRoute() {
+      if (this.$route.name) {
+        return this.$route;
+      }
+      return false;
+    }
+  },
+  watch: {
+    $route() {
+      this.addViewTabs();
+    }
+  }
 };
 </script>
 
--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,7 @@
 import router from './router'
 import NProgress from 'nprogress' // Progress 进度条
 import 'nprogress/nprogress.css' // Progress 进度条样式
+// import { Message } from 'element-ui' // 错误提示
 
 // 路由
 router.beforeEach((to, from, next) => {
