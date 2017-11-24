4c330e0806c91b1fb41e11b61433dcfa2a6037ad
xiaolin <462441355@qq.com>
2017-11-24 15:27:16 +0800

    * 初始化

--- src/components/excel/index.vue
+++ src/components/excel/index.vue
@@ -1,15 +1,37 @@
 <template>
 	<div class="app-container">
-    <el-button style='margin-bottom:20px;' type="primary" icon="document" @click="handleDownload" :loading="downloadLoading">导出excel</el-button>
+    <el-button style='margin-bottom:20px;' type="primary" icon="document"
+              @click="handleDownload"
+              :loading="downloadLoading">导出excel</el-button>
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="拼命加载中" border fit highlight-current-row>
+
+    </el-table>
   </div>
 </template>
 
 <script>
-  export default {
-    methods:{
-      handleDownload(){
-
-      }
+import { shopList } from "@/api/shop";
+export default {
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      downloadLoading: false
+    };
+  },
+  created() {
+    this.getShopList();
+  },
+  methods: {
+    handleDownload() {},
+    // 获取shop数据
+    getShopList() {
+      this.listLoading = true
+      shopList().then(response => {
+        this.list = response.data.data
+        this.listLoading = false
+      });
     }
   }
+};
 </script>
