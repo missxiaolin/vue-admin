e0052ee831ac88c0dc543baace8edc423896d170
xiaolin <462441355@qq.com>
2017-11-30 21:02:48 +0800

    * 增加token

--- src/api/shop.js
+++ src/api/shop.js
@@ -4,6 +4,10 @@ import fetch from '@/common/js/fetch'
  * shop列表
  * @param {*} query
  */
-export function shopList (query) {
-  return fetch({url: '/v1/shop/list', method: 'get', params: query})
+export function shopList(query) {
+  return fetch({
+    url: '/v1/shop/list',
+    method: 'get',
+    params: query
+  })
 }
--- src/common/js/fetch.js
+++ src/common/js/fetch.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
+import { getToken } from 'common/js/cache'
 
 // 创建axios实例
 const service = axios.create({
@@ -11,8 +12,13 @@ const service = axios.create({
 // request拦截器
 service.interceptors.request.use(config => {
   // Do something before request is sent
-  if (store.getters.token) {
+  let token = getToken()
+  if (token) {
+    config.headers['X-TOKEN'] = token
   }
+
+  // config.headers['Content-Type'] = 'application/json'
+  // config.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8'
   return config
 }, error => {
   // Do something with request error
@@ -25,33 +31,6 @@ service.interceptors.response.use(
   response => {
     return response
   },
-  /**
-  * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
-  * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
-  */
-//  const res = response.data;
-//     if (res.code !== 20000) {
-//       Message({
-//         message: res.message,
-//         type: 'error',
-//         duration: 5 * 1000
-//       });
-//       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-//       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-//         MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-//           confirmButtonText: '重新登录',
-//           cancelButtonText: '取消',
-//           type: 'warning'
-//         }).then(() => {
-//           store.dispatch('FedLogOut').then(() => {
-//             location.reload();// 为了重新实例化vue-router对象 避免bug
-//           });
-//         })
-//       }
-//       return Promise.reject('error');
-//     } else {
-//       return response.data;
-//     }
   error => {
     console.log('err' + error)// for debug
     Message({
